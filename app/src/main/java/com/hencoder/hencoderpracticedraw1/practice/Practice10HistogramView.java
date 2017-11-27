0911cbbca03536b37384a19983ce4034557b2832
pexjl12310@163.com <pexjl12310@163.com>
2017-11-27 22:48:07 +0800

    做课后练习题

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
@@ -2,11 +2,19 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice10HistogramView extends View {
+
+    private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private Path path = new Path();
+    private String[] strings = new String[]{"Froyo", "GB", "ICS", "JB", "KitKat", "L", "M"};
+    private int[] values = new int[]{0, 5, 5, 15, 30, 35, 10};
 
     public Practice10HistogramView(Context context) {
         super(context);
@@ -26,5 +34,22 @@ public class Practice10HistogramView extends View {
 
 //        综合练习
 //        练习内容：使用各种 Canvas.drawXXX() 方法画直方图
+        path.moveTo(100, 50);
+        path.lineTo(100, 600);
+        path.rLineTo(900, 0);
+        paint.setStrokeWidth(10);
+        paint.setColor(Color.WHITE);
+        paint.setStyle(Paint.Style.STROKE);
+        canvas.drawPath(path, paint);
+        for (int i = 0; i < strings.length; i++) {
+            paint.setStyle(Paint.Style.FILL);
+            paint.setColor(Color.WHITE);
+            paint.setTextSize(30);
+            paint.setTextAlign(Paint.Align.CENTER);
+            canvas.drawText(strings[i], 120 * i + 150, 650, paint);
+            paint.setColor(Color.GREEN);
+            paint.setStyle(Paint.Style.FILL);
+            canvas.drawRect(120 * i + 120, (1 - values[i] / 100f) * 600, 120 * i + 170, 600, paint);
+        }
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice9DrawPathView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice9DrawPathView.java
@@ -2,11 +2,17 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice9DrawPathView extends View {
+
+    private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+    private Path path = new Path();
 
     public Practice9DrawPathView(Context context) {
         super(context);
@@ -25,5 +31,11 @@ public class Practice9DrawPathView extends View {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawPath() 方法画心形
+        paint.setStyle(Paint.Style.FILL);
+        path.arcTo(200, 200, 600, 600, 180, 180, true);
+        path.arcTo(600, 200, 1000, 600, 180, 180, true);
+        path.lineTo(600, 1000);
+        path.lineTo(200, 400);
+        canvas.drawPath(path, paint);
     }
 }
