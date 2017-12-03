25660026d7a8de37b92eeda343e91c80cc820b38
pexjl12310@163.com <pexjl12310@163.com>
2017-12-03 11:37:55 +0800

    增加饼图练习的答案

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice11PieChartView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice11PieChartView.java
@@ -2,11 +2,18 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice11PieChartView extends View {
+
+    private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+    private RectF rectF = new RectF(200, 200, 600, 600);
 
     public Practice11PieChartView(Context context) {
         super(context);
@@ -26,5 +33,17 @@ public class Practice11PieChartView extends View {
 
 //        综合练习
 //        练习内容：使用各种 Canvas.drawXXX() 方法画饼图
+        paint.setColor(Color.YELLOW);
+        canvas.drawArc(rectF, 0, -70, true, paint);
+        paint.setColor(Color.RED);
+        canvas.drawArc(180, 180, 580, 580, 180, 110, true, paint);
+        paint.setColor(Color.BLUE);
+        canvas.drawArc(rectF, 77, 100, true, paint);
+        paint.setColor(Color.GREEN);
+        canvas.drawArc(rectF, 75, -50, true, paint);
+        paint.setColor(Color.GRAY);
+        canvas.drawArc(rectF, 25, -15, true, paint);
+        paint.setColor(Color.CYAN);
+        canvas.drawArc(rectF, 14, -14, true, paint);
     }
 }
